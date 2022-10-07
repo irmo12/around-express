85624f66c607bad13c0887469dc5cf7740118249
irmo12 <omriws@hushmail.com>
2022-10-07 17:57:07 +0300

    implement users/id  fetch

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,16 @@ router.get("/", (req, res) => {
   fs.readFile(dataPath, { encoding: "utf8" })
     .then((users) => res.send({ data: JSON.parse(users) }))
     .catch(() => {
-      res.status(500).send({ "message": "Internal server error" });
+      res.status(500).send({ "message": "Internal server error1" });
     });
 });
 
 router.get("/:id", (req, res) => {
+  const { id } = req.params;
   fs.readFile(dataPath, { encoding: "utf8" })
     .then((users) => {
-      const user = (users).find((user) => user._id === req.params.id);
+      const data = JSON.parse(users);
+      const user = (data).find((user) => user._id === id);
 
       if (!user) {
         res.status(404).send({ "message": "User ID not found" });
